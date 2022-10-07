cc3b73dc8ddec2ef18477e9cf7032852ce3cf592
Bhupendra Deshmukh <87089425+bhupend-deshmukh@users.noreply.github.com>
2022-10-07 09:50:54 +0530

    Add files via upload
    crud hapi js

---
+++ knexfile.js
@@ -0,0 +1,50 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'mysql2',
+    connection: {
+      user:"bhupendra",
+      host:"localhost",
+      password:"Bhupendra@123",
+      database:"learn_hapi",
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+    }
+
+};
